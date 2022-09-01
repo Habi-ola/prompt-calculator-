c04c032f9ad52127691e0a8961bf4d49b79e6ff6
Abiola <habioladokun@gmail.com>
2022-09-01 23:24:43 +0100

    first commit

---
+++ pcalc.js
@@ -0,0 +1,9 @@
+var w = prompt("Type your first number");
+var x = prompt("Type your second number")
+var y = prompt("which operation would you like to perform: choose one * for multiplication; + for addition;")
+if (y == "*") {
+    let text = Number(w) * Number(x)
+    alert(text)
+}
+
+
