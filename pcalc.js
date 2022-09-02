e81ce0652be292605df089f9c57e748346b4cff7
Habi-ola <habioladokun@gmail.com>
2022-09-02 16:49:02 +0100

    first commit

--- pcalc.js
+++ pcalc.js
@@ -1,9 +1,19 @@
 var w = prompt("Type your first number");
 var x = prompt("Type your second number")
-var y = prompt("which operation would you like to perform: choose one * for multiplication; + for addition;")
+var y = prompt("which operation would you like to perform: choose one * for multiplication; + for addition; - for subtraction; / for division")
 if (y == "*") {
     let text = Number(w) * Number(x)
     alert(text)
 }
-
-
+if (y == "+") {
+    let text = Number(w) + Number(x)
+    alert(text)
+}
+else if (y == "-") {
+    let text = Number(w) - Number(x)
+    alert(text)
+}
+else if (y == "/") {
+    let text = Number(w) / Number(x)
+    alert(text)
+}
